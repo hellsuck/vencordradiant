927e6c0ab80014a4d7d29f2bfe5bdc9d87bcedcc
Vince <vince@vivian.lol>
2024-11-19 14:31:40 +0100

    Remove update check

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -91,35 +91,9 @@ async function init() {
 
     syncSettings();
 
-    if (!IS_WEB && !IS_UPDATER_DISABLED) {
-        try {
-            const isOutdated = await checkForUpdates();
-            if (!isOutdated) return;
 
-            if (Settings.autoUpdate) {
-                await update();
-                if (Settings.autoUpdateNotification)
-                    setTimeout(() => showNotification({
-                        title: "Vencord has been updated!",
-                        body: "Click here to restart",
-                        permanent: true,
-                        noPersist: true,
-                        onClick: relaunch
-                    }), 10_000);
-                return;
-            }
 
-            setTimeout(() => showNotification({
-                title: "A Vencord update is available!",
-                body: "Click here to view the update",
-                permanent: true,
-                noPersist: true,
-                onClick: openUpdaterModal!
-            }), 10_000);
-        } catch (err) {
-            UpdateLogger.error("Failed to check for updates", err);
-        }
-    }
+
 
     if (IS_DEV) {
         const pendingPatches = patches.filter(p => !p.all && p.predicate?.() !== false);
